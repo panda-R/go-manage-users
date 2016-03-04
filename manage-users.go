6ff1c6c20805c97ba3bd443cf5c8e47dca345dad
Wallace Choi <wallace.choi@calgaryscientific.com>
2016-03-04 10:21:12 -0700

    Added OS runtime detection and some functions

--- manage-users.go
+++ manage-users.go
@@ -2,22 +2,28 @@ package main
 
 import "fmt"
 import "encoding/base64"
-
 import "os"
+import "os/exec"
 import "io"
 import "strings"
 import "path/filepath"
-
 import "crypto/sha1"
 import "regexp"
-
 import "io/ioutil"
 import "github.com/davidgamba/go-getoptions"
+import "runtime"
 
-var user_file_dir = string(filepath.Separator) + "tmp" + string(filepath.Separator) + "CSI"
+var user_file_dir = string(filepath.Separator) + "CSI" + string(filepath.Separator) + "PureWeb" + string(filepath.Separator) + "Server" + string(filepath.Separator) + "webapp" + string(filepath.Separator) + "WEB-INF"
+
 var user_service_file = "user-service.properties"
+var manpage_filename = "manage-users.1"
 
 func main() {
+
+	if runtime.GOOS == "linux" {
+		user_file_dir = string(filepath.Separator) + "opt" + user_file_dir
+		fmt.Println(user_file_dir)
+	}
 
 	opt := getoptions.GetOptions()
 	var list bool
@@ -28,6 +34,7 @@ func main() {
 	var delete string
 	var csv string
 	var help bool
+	var h bool
 
 	opt.BoolVar(&list, "list", false, "l", "panda")
 	opt.StringVar(&dir, "dir", "")
@@ -36,7 +43,8 @@ func main() {
 	opt.StringVar(&role, "role", "enduser", "r")
 	opt.StringVar(&delete, "delete", "", "d")
 	opt.StringVar(&csv, "csv", "")
-	opt.BoolVar(&help, "help", false, "h")
+	opt.BoolVar(&help, "help", false)
+	opt.BoolVar(&h, "h", false)
 
 	remaining, err := opt.Parse(os.Args[1:])
 
@@ -48,55 +56,96 @@ func main() {
 		fmt.Printf("Remaining: %s\n, remaining")
 	}
 
-	if opt.Called["dir"] {
-		user_file_dir = dir
-	}
-
-	writer, err := os.OpenFile(user_file_dir+string(filepath.Separator)+user_service_file, os.O_RDWR|os.O_APPEND, 0666)
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Printf("[INFO] File Doesn't Exist\n")
-			os.MkdirAll(user_file_dir, 0755)
-			writer, err := os.Create(user_file_dir + string(filepath.Separator) + user_service_file)
-			check(err)
-			writer.Close()
-		}
-	}
-	writer.Close()
-
-	fmt.Println("[INFO] Using the following user file: " + user_file_dir + string(filepath.Separator) + user_service_file)
-
-	if opt.Called["user"] {
-		// check that password is also passed through
-		if opt.Called["password"] {
-			add_user_to_user_service_file(user, password, role, true)
-		} else {
-			fmt.Println("[INFO] Password must also be set.")
-		}
-	}
-
-	if opt.Called["delete"] {
-		remove_user_from_user_service_file(delete)
-	}
-	if opt.Called["list"] {
-		get_users_from_user_service_file()
-	}
-	if opt.Called["csv"] {
-		add_users_from_file(csv)
-	}
-
-	if opt.Called["help"] {
+	if len(opt.Called) == 0 || opt.Called["h"] {
 		print_help_synopsis()
+	} else if opt.Called["help"] {
+		print_man_page()
+	} else {
+
+		if opt.Called["dir"] {
+			user_file_dir = dir
+		}
+
+		writer, err := os.OpenFile(user_file_dir+string(filepath.Separator)+user_service_file, os.O_RDWR|os.O_APPEND, 0666)
+		if err != nil {
+			if os.IsNotExist(err) {
+				fmt.Printf("[INFO] File Doesn't Exist\n")
+				os.MkdirAll(user_file_dir, 0755)
+				writer, err := os.Create(user_file_dir + string(filepath.Separator) + user_service_file)
+				check(err)
+				writer.Close()
+			}
+
+		}
+		writer.Close()
+
+		fmt.Println("[INFO] Using the following user file: " + user_file_dir + string(filepath.Separator) + user_service_file)
+
+		if opt.Called["user"] {
+			// check that password is also passed through
+			if opt.Called["password"] {
+				add_user_to_user_service_file(user, password, role, true)
+			} else {
+				fmt.Println("[INFO] Password must also be set.")
+			}
+		}
+
+		if opt.Called["delete"] {
+			remove_user_from_user_service_file(delete)
+		}
+		if opt.Called["list"] {
+			get_users_from_user_service_file()
+		}
+		if opt.Called["csv"] {
+			add_users_from_file(csv)
+		}
 	}
 
 }
 
+func print_man_page() {
+	if runtime.GOOS == "linux" {
+		if _, err := os.Stat(manpage_filename); os.IsNotExist(err) {
+			fmt.Println("[WARNING] full man page is missing")
+			print_help_synopsis()
+		} else {
+			cmd := exec.Command("man", "-l", manpage_filename)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Run()
+		}
+	}
+}
+
 func print_help_synopsis() {
-	fmt.Printf("# List users\nmanage-users -l | --list\n\t\t[--dir <users_file_dir>]\n\n")
-	fmt.Printf("# Delete Single Users\nmanage-users -d | --delete\n\t\t[--dir <users_file_dir>]\n\n")
-	fmt.Printf("# Add Single Users\nmanage-users -u | --user <username>\n\t\t-p | --password <password>\n\t\t[-r | --role <enduser|monitor|admin>]\n\t\t[--dir <users_file_dir>]\n\n")
-	fmt.Printf("#Add users from csv file (Format: user,password,role)\nmanage-users --csv <csv_file>\n\t\t[--dir <users_file_dir>]")
-	fmt.Printf("# show this message\nmanage-users -h")
+	fmt.Printf(`
+# List users 
+manage-users 	-l | --list
+				[--dir <users_file_dir>]
+
+# Delete Single Users
+manage-users	-d | --delete
+				[--dir <users_file_dir>]
+# Add Single Users 
+manage-users	-u | --user <username>
+				-p | --password <password>
+				[-r | --role <enduser|monitor|admin>]
+				[--dir <users_file_dir>]
+				
+#Add users from csv file (Format: user,password,role)
+manage-users	--csv <csv_file> 
+				[--dir <users_file_dir>]
+
+# show this message
+manage-users -h
+	` + "\n")
+
+	if runtime.GOOS == "linux" {
+		fmt.Printf(`
+# show man page
+manage-users --help
+	` + "\n")
+	}
 }
 
 func add_users_from_file(filename string) {
@@ -118,7 +167,9 @@ func get_users_from_user_service_file() {
 	lines := strings.Split(string(input), "\n")
 	for _, line := range lines {
 		username := strings.Split(string(line), "=")
-		fmt.Println(string(username[0]))
+		if username[0] != "" {
+			fmt.Println(string(username[0]))
+		}
 	}
 }
 
@@ -160,7 +211,7 @@ func add_user_to_user_service_file(username string, password string, user_type s
 	buf, err := ioutil.ReadFile(user_file_dir + string(filepath.Separator) + user_service_file)
 	check(err)
 	s := string(buf)
-	regex, err := regexp.Compile(username)
+	regex, err := regexp.Compile(username + "=")
 	check(err)
 	if regex.MatchString(s) {
 		panic("User already exists: " + username)
